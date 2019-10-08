9c053415509b58fe4c95d43f051847e75ece39dd
Jessica <jessica.keune@ugent.be>
2019-10-08 09:13:13 +0200

    Removing progress.bar and joblib packages

--- metfunctions.py
+++ metfunctions.py
@@ -22,13 +22,13 @@ import sys
 import time
 import random
 import multiprocessing    
-from joblib import Parallel, delayed
+#from joblib import Parallel, delayed
 from copy import deepcopy
 from datetime import datetime, timedelta
 from math import sin,cos,acos,atan,atan2,sqrt
 from dateutil.relativedelta import relativedelta
 import datetime as datetime
-from progress.bar import Bar
+#from progress.bar import Bar
 
 ###############################################################################
 
