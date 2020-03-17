chore: adding plots of scaling factors and bias-corrected fields to fdebug = True option

03_biascorrection.py
@@ -189,9 +189,9 @@ def main_biascorrection(
         mask = f['mask'][:]
         mlat = f['lat'][:]
         mlon = f['lon'][:]   
-    #if verbose: basicplot(mask, mlat, mlon, title="actual masks (all ecoregions)")
+    if fdebug: basicplot(mask, mlat, mlon, title="actual masks (all ecoregions)")
     mask[(mask>0) & (mask!=1)] = 0 ## NOTE: HARDCODED FOR ECOREGION 1 (NGP)
-    #if verbose: basicplot(mask, mlat, mlon, title="actual mask (NGP only)")
+    if fdebug: basicplot(mask, mlat, mlon, title="actual mask (NGP only)")
     
     
     ## area-weight arrival region precipitation (FLEXPART & REF)
@@ -249,14 +249,14 @@ def main_biascorrection(
     # 3.) alright, now calculate how much more scaling is needed to match P too
     f_remain = Pratio / f_Escaled
     
-#    if verbose:
-#        from matplotlib import pyplot as plt
-#        plt.figure
-#        plt.plot(f_remain, label="remaining scaling factor")
-#        plt.plot(Pratio, label="P ratio (REF/FLEX)")
-#        plt.legend()
-#        plt.title("note: ratio is negative because FLEX-P is <0")
-#        plt.show()    
+    if fdebug:
+        from matplotlib import pyplot as plt
+        plt.figure
+        plt.plot(f_remain, label="remaining scaling factor")
+        plt.plot(Pratio, label="P ratio (REF/FLEX)")
+        plt.legend()
+        plt.title("note: ratio is negative because FLEX-P is <0")
+        plt.show()    
     
     #******************************************************************************
     ## swap axes to enable numpy broadcasting; 
@@ -278,21 +278,19 @@ def main_biascorrection(
     E2P_Pscaled  = np.nansum(E2P_Pscaled, axis=1)
     E2P_EPscaled = np.nansum(E2P_EPscaled, axis=1)
     
-#    if verbose:
-#        
-#        basicplot(np.nanmean(Had, axis=0), lats, lons, 
-#                  title="raw Had, daily mean")
-#        basicplot(np.nanmean(Had_scaled, axis=0), lats, lons, 
-#                  title="H-scaled Had, daily mean")
-#    
-#        basicplot(np.nanmean(E2P, axis=0), lats, lons, 
-#                  title="raw E2P, daily mean")
-#        basicplot(np.nanmean(E2P_Escaled, axis=0), lats, lons, 
-#                  title="E-scaled E2P, daily mean")    
-#        basicplot(np.nanmean(E2P_Pscaled, axis=0), lats, lons, 
-#                  title="P-scaled E2P, daily mean")
-#        basicplot(np.nanmean(E2P_EPscaled, axis=0), lats, lons, 
-#                  title="E-P-scaled E2P, daily mean")
+    if fdebug:
+        basicplot(np.nanmean(Had, axis=0), lats, lons, 
+                  title="raw Had, daily mean")
+        basicplot(np.nanmean(Had_scaled, axis=0), lats, lons, 
+                  title="H-scaled Had, daily mean")
+        basicplot(np.nanmean(E2P, axis=0), lats, lons, 
+                  title="raw E2P, daily mean")
+        basicplot(np.nanmean(E2P_Escaled, axis=0), lats, lons, 
+                  title="E-scaled E2P, daily mean")    
+        basicplot(np.nanmean(E2P_Pscaled, axis=0), lats, lons, 
+                  title="P-scaled E2P, daily mean")
+        basicplot(np.nanmean(E2P_EPscaled, axis=0), lats, lons, 
+                  title="E-P-scaled E2P, daily mean")
     
     
     ##--6. save output ############################################################