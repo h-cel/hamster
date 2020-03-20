5ca7f00161012042811eac8548841429a051b3c6
Jessica <jessica.keune@ugent.be>
2020-03-20 17:24:09 +0100

    fix: typo in lon2 for jump calculation (was using lon1 instead of lon2)

--- 01_diagnosis.py
+++ 01_diagnosis.py
@@ -149,7 +149,7 @@ def main_diagnosis(
         for i in ntot:
 
             ## check for jumps 
-            jump = dist_on_sphere(ary[0,i,2],ary[0,i,1],ary[1,i,2],ary[0,i,1]) #lat1,lon1,lat2,lon2
+            jump = dist_on_sphere(ary[0,i,2],ary[0,i,1],ary[1,i,2],ary[1,i,1]) #lat1,lon1,lat2,lon2
             if jump > cjumps:
                 njumps += int(1)
                 continue
