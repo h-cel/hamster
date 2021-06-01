27e283a6abb2fcc9a5e622eb636869991811b6c8
Jessica <jessica.keune@ugent.be>
2021-06-01 10:45:23 +0200

    docs: adding src refs

--- setup.py
+++ setup.py
@@ -27,4 +27,7 @@ setup(
                 # 'Programming Language :: Python :: 3.6',
                 # 'Programming Language :: Python :: 3.7',
               ],
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src"),
+        python_requires=">=3.6",
 )
