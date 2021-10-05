d80fe725e9d0d11245b51241b85f3450688ac505
Jessica <jessica.keune@ugent.be>
2021-10-05 20:54:31 +0200

    fix: random.choices

--- src/hamsterfunctions.py
+++ src/hamsterfunctions.py
@@ -1728,7 +1728,7 @@ def random_attribution_p(
             if veryverbose:
                 print("  *** -- enforcing attribution to uptake location " + str(ii))
         else:
-            ii = random.choice(iupt, weights=lweights, k=1)
+            ii = random.choices(iupt, weights=lweights, k=1)
         # determine maximum attribution for current uptake location and iteration
         try:
             imin = np.argmin(qtot[1:ii]) + 1
